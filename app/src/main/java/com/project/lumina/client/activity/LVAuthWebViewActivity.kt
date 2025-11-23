9bd4e9f3cf255538b43065672b46d68010e2d5c9
THeLovelySheep <116801545+THeLovelySheep@users.noreply.github.com>
2025-11-23 22:37:27 +0800

    更新 LVAuthWebViewActivity.kt

--- app/src/main/java/com/project/lumina/client/activity/LVAuthWebViewActivity.kt
+++ app/src/main/java/com/project/lumina/client/activity/LVAuthWebViewActivity.kt
@@ -7,7 +7,6 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.project.lumina.client.util.SessionManager
 
 class LVAuthWebViewActivity : AppCompatActivity() {
 
@@ -40,8 +39,6 @@ class LVAuthWebViewActivity : AppCompatActivity() {
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 val url = request?.url.toString()
                 if (url.contains("projectlumina.online")) {
-                    val sessionManager = SessionManager(applicationContext)
-                    sessionManager.saveSession()
                     val packageManager = applicationContext.packageManager
                     val intent = packageManager.getLaunchIntentForPackage(applicationContext.packageName)
                     intent?.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
